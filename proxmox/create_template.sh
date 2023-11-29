0ab092edcd8cc6d270084e2e941c652b70230112
liexner <liexner@gmail.com>
2023-11-29 08:59:15 +0100

    Create create_template.sh

---
+++ proxmox/create_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <image_link> <vm_id>"
+    exit 1
+fi
+
+image_link="$1"
+vm_id="$2"
+
+wget "$image_link" -O "cloud-image-$vm_id.img"
+apt update
+apt install -y libguestfs-tools
+virt-customize --install qemu-guest-agent -a jammy-server-cloudimg-amd64.img
+virt-customize --install nfs-common -a jammy-server-cloudimg-amd64.img
+
+qm create 9000 --name ubuntu22-template --memory 4096 --net0 virtio,bridge=vmbr0
+import_output=$(qm importdisk 9000 jammy-server-cloudimg-amd64.img local)
+echo "$import_output"
