1111de7ae956bc0ca5fed7ce01210d6eda4f8cd4
liexner <liexner@gmail.com>
2023-11-29 09:02:22 +0100

    Update create_template.sh

--- proxmox/create_template.sh
+++ proxmox/create_template.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <image_link> <vm_id>"
+#curl -s https://raw.githubusercontent.com/liexner/homelab/main/proxmox/create_template.sh | bash -s https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img 9000 ubuntu-test
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <image_link> <vm_id> <template_name>"
     exit 1
 fi
 
 image_link="$1"
 vm_id="$2"
+template_name="$3"
 
 wget "$image_link" -O "cloud-image-$vm_id.img"
 apt update
